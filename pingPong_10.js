43dca1d91ae19cdb5e0ef8fa59e31f690a05ade9
zannatul mawa <zannatulmawa38@gmail.com>
2026-01-08 19:24:17 +0600

    commiting

--- pingPong_10.js
+++ pingPong_10.js
@@ -1,9 +1,16 @@
 function PingPong() {
     let num = 20;
     for (let i = 0; i < num; i++) {
-        console.log(i)
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("PingPong");
+        } else if (i % 3 === 0) {
+            console.log("Ping");
+        } else if (i % 5 === 0) {
+            console.log("Pong");
+        } else {
+            console.log(i);
+        }
     }
-
     return num;
 }
 
